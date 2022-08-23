0ddad6e81aad1260f5d2f88ac67ba5e1c637b7ca
AdemolaOlaoti <105083847+AdemolaOlaoti@users.noreply.github.com>
2022-08-23 14:31:57 -0400

    Update 8.eks.tf

--- 8.eks.tf
+++ 8.eks.tf
@@ -20,22 +20,22 @@ terraform {
 # Step 2 - Create a VPC
 #======================
 
-resource "aws_vpc" "main" {
+resource "aws_vpc" "techdomvpc" {
   cidr_block = "10.0.0.0/16"
 
   tags = {
-    Name = "main"
+    Name = "techdomvpc"
   }
 }
 
 # Step 3 - Create an Internet Gateway
 #====================================
 
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.main.id
+resource "aws_internet_gateway" "techdomigw" {
+  vpc_id = aws_vpc.techdomvpc.id
 
   tags = {
-    Name = "igw"
+    Name = "techdomigw"
   }
 }
 
@@ -43,76 +43,77 @@ resource "aws_internet_gateway" "igw" {
 #========================
 
 resource "aws_subnet" "private-us-east-1a" {
-  vpc_id            = aws_vpc.main.id
+  vpc_id            = aws_vpc.techdomvpc.id
   cidr_block        = "10.0.0.0/19"
   availability_zone = "us-east-1a"
 
   tags = {
-    "Name"                            = "private-us-east-1a"
-    "kubernetes.io/role/internal-elb" = "1"
-    "kubernetes.io/cluster/demo"      = "owned"
+    "Name"                                      = "private-us-east-1a"
+    "kubernetes.io/role/internal-elb"           = "1"
+    "kubernetes.io/cluster/techdomcluster"      = "owned"
   }
 }
 
 resource "aws_subnet" "private-us-east-1b" {
-  vpc_id            = aws_vpc.main.id
+  vpc_id            = aws_vpc.techdomvpc.id
   cidr_block        = "10.0.32.0/19"
   availability_zone = "us-east-1b"
 
   tags = {
-    "Name"                            = "private-us-east-1b"
-    "kubernetes.io/role/internal-elb" = "1"
-    "kubernetes.io/cluster/demo"      = "owned"
+    "Name"                                      = "private-us-east-1b"
+    "kubernetes.io/role/internal-elb"           = "1"
+    "kubernetes.io/cluster/techdomcluster"      = "owned"
   }
 }
 
 resource "aws_subnet" "public-us-east-1a" {
-  vpc_id                  = aws_vpc.main.id
+  vpc_id                  = aws_vpc.techdomvpc.id
   cidr_block              = "10.0.64.0/19"
   availability_zone       = "us-east-1a"
   map_public_ip_on_launch = true
 
   tags = {
-    "Name"                       = "public-us-east-1a"
-    "kubernetes.io/role/elb"     = "1"
-    "kubernetes.io/cluster/demo" = "owned"
+    "Name"                                 = "public-us-east-1a"
+    "kubernetes.io/role/elb"               = "1"
+    "kubernetes.io/cluster/techdomcluster" = "owned"
   }
 }
 
 resource "aws_subnet" "public-us-east-1b" {
-  vpc_id                  = aws_vpc.main.id
+  vpc_id                  = aws_vpc.techdomvpc.id
   cidr_block              = "10.0.96.0/19"
   availability_zone       = "us-east-1b"
   map_public_ip_on_launch = true
 
   tags = {
-    "Name"                       = "public-us-east-1b"
-    "kubernetes.io/role/elb"     = "1"
-    "kubernetes.io/cluster/demo" = "owned"
+    "Name"                                 = "public-us-east-1b"
+    "kubernetes.io/role/elb"               = "1"
+    "kubernetes.io/cluster/techdomcluster" = "owned"
   }
 }
 
 
 # Step 5 - Create NAT (Network Address Translation) Gateway
 #==========================================================
+# Understanding NAT Gateway- https://docs.aws.amazon.com/vpc/latest/userguide/vpc-nat-gateway.html
 
-resource "aws_eip" "nat" {
+resource "aws_eip" "techdomnat" {
   vpc = true
 
   tags = {
-    Name = "nat"
+    Name = "techdomnat"
   }
 }
 
-resource "aws_nat_gateway" "nat" {
-  allocation_id = aws_eip.nat.id
+resource "aws_nat_gateway" "techdomnat" {
+  allocation_id = aws_eip.techdomnat.id
   subnet_id     = aws_subnet.public-us-east-1a.id
 
   tags = {
-    Name = "nat"
+    Name = "techdomnat"
   }
 
-  depends_on = [aws_internet_gateway.igw]
+  depends_on = [aws_internet_gateway.techdomigw]
 }
 
 
@@ -120,12 +121,12 @@ resource "aws_nat_gateway" "nat" {
 #=================================================================
 
 resource "aws_route_table" "private" {
-  vpc_id = aws_vpc.main.id
+  vpc_id = aws_vpc.techdomvpc.id
 
   route = [
     {
       cidr_block                 = "0.0.0.0/0"
-      nat_gateway_id             = aws_nat_gateway.nat.id
+      nat_gateway_id             = aws_nat_gateway.techdomnat.id
       carrier_gateway_id         = ""
       destination_prefix_list_id = ""
       egress_only_gateway_id     = ""
@@ -146,12 +147,12 @@ resource "aws_route_table" "private" {
 }
 
 resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.main.id
+  vpc_id = aws_vpc.techdomvpc.id
 
   route = [
     {
       cidr_block                 = "0.0.0.0/0"
-      gateway_id                 = aws_internet_gateway.igw.id
+      gateway_id                 = aws_internet_gateway.techdomigw.id
       nat_gateway_id             = ""
       carrier_gateway_id         = ""
       destination_prefix_list_id = ""
@@ -195,8 +196,8 @@ resource "aws_route_table_association" "public-us-east-1b" {
 # Step 7 - Create IAM Role for the Cluster, Attach Policy to Role, and Create a Cluster
 #======================================================================================
 
-resource "aws_iam_role" "demo" {
-  name = "eks-cluster-demo"
+resource "aws_iam_role" "techdomclusterrole" {
+  name = "techdomclusterrole"
 
   assume_role_policy = <<POLICY
 {
@@ -214,14 +215,14 @@ resource "aws_iam_role" "demo" {
 POLICY
 }
 
-resource "aws_iam_role_policy_attachment" "demo-AmazonEKSClusterPolicy" {
+resource "aws_iam_role_policy_attachment" "techdomclusterrole-AmazonEKSClusterPolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = aws_iam_role.demo.name
+  role       = aws_iam_role.techdomclusterrole.name
 }
 
-resource "aws_eks_cluster" "demo" {
-  name     = "demo"
-  role_arn = aws_iam_role.demo.arn
+resource "aws_eks_cluster" "techdomcluster" {
+  name     = "techdomcluster"
+  role_arn = aws_iam_role.techdomclusterrole.arn
 
   vpc_config {
     subnet_ids = [
@@ -232,15 +233,15 @@ resource "aws_eks_cluster" "demo" {
     ]
   }
 
-  depends_on = [aws_iam_role_policy_attachment.demo-AmazonEKSClusterPolicy]
+  depends_on = [aws_iam_role_policy_attachment.techdomclusterrole-AmazonEKSClusterPolicy]
 }
 
 
 # Step 8 - Create IAM Role for the Worker Nodes, Attach Policies to Role, and Create Worker Nodes 
 #================================================================================================
 
-resource "aws_iam_role" "nodes" {
-  name = "eks-node-group-nodes"
+resource "aws_iam_role" "techdomnoderole" {
+  name = "techdomnodes"
 
   assume_role_policy = jsonencode({
     Statement = [{
@@ -254,25 +255,25 @@ resource "aws_iam_role" "nodes" {
   })
 }
 
-resource "aws_iam_role_policy_attachment" "nodes-AmazonEKSWorkerNodePolicy" {
+resource "aws_iam_role_policy_attachment" "techdomnoderole-AmazonEKSWorkerNodePolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-  role       = aws_iam_role.nodes.name
+  role       = aws_iam_role.techdomnoderole.name
 }
 
-resource "aws_iam_role_policy_attachment" "nodes-AmazonEKS_CNI_Policy" {
+resource "aws_iam_role_policy_attachment" "techdomnoderole-AmazonEKS_CNI_Policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-  role       = aws_iam_role.nodes.name
+  role       = aws_iam_role.techdomnoderole.name
 }
 
-resource "aws_iam_role_policy_attachment" "nodes-AmazonEC2ContainerRegistryReadOnly" {
+resource "aws_iam_role_policy_attachment" "techdomnoderole-AmazonEC2ContainerRegistryReadOnly" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-  role       = aws_iam_role.nodes.name
+  role       = aws_iam_role.techdomnoderole.name
 }
 
-resource "aws_eks_node_group" "private-nodes" {
-  cluster_name    = aws_eks_cluster.demo.name
-  node_group_name = "private-nodes"
-  node_role_arn   = aws_iam_role.nodes.arn
+resource "aws_eks_node_group" "techdomnodes" {
+  cluster_name    = aws_eks_cluster.techdomcluster.name
+  node_group_name = "techdomnodes"
+  node_role_arn   = aws_iam_role.techdomnoderole.arn
 
   subnet_ids = [
     aws_subnet.private-us-east-1a.id,
@@ -280,16 +281,16 @@ resource "aws_eks_node_group" "private-nodes" {
   ]
 
   capacity_type  = "ON_DEMAND"
-  instance_types = ["t3.small"]
+  instance_types = ["t2.micro"]
 
   scaling_config {
-    desired_size = 1
-    max_size     = 5
-    min_size     = 0
+    desired_size = 2
+    max_size     = 3
+    min_size     = 1
   }
 
   update_config {
-    max_unavailable = 1
+    max_unavailable = 2
   }
 
   labels = {
@@ -308,9 +309,9 @@ resource "aws_eks_node_group" "private-nodes" {
   # }
 
   depends_on = [
-    aws_iam_role_policy_attachment.nodes-AmazonEKSWorkerNodePolicy,
-    aws_iam_role_policy_attachment.nodes-AmazonEKS_CNI_Policy,
-    aws_iam_role_policy_attachment.nodes-AmazonEC2ContainerRegistryReadOnly,
+    aws_iam_role_policy_attachment.techdomnoderole-AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.techdomnoderole-AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.techdomnoderole-AmazonEC2ContainerRegistryReadOnly,
   ]
 }
 
@@ -330,47 +331,47 @@ resource "aws_eks_node_group" "private-nodes" {
 # }
 
 # Step 9 - Create an Open ID Connection for the Cluster
-#==================================================
+#======================================================
 
-data "tls_certificate" "eks" {
-  url = aws_eks_cluster.demo.identity[0].oidc[0].issuer
+data "tls_certificate" "techdomeks" {
+  url = aws_eks_cluster.techdomcluster.identity[0].oidc[0].issuer
 }
 
-resource "aws_iam_openid_connect_provider" "eks" {
+resource "aws_iam_openid_connect_provider" "techdomeks" {
   client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = [data.tls_certificate.eks.certificates[0].sha1_fingerprint]
-  url             = aws_eks_cluster.demo.identity[0].oidc[0].issuer
+  thumbprint_list = [data.tls_certificate.techdomeks.certificates[0].sha1_fingerprint]
+  url             = aws_eks_cluster.techdomcluster.identity[0].oidc[0].issuer
 }  
 
 
 # Step 10 - Create IAM Role for the Open-ID Connection, and Attach Policy to Role
 #================================================================================
 
-data "aws_iam_policy_document" "test_oidc_assume_role_policy" {
+data "aws_iam_policy_document" "techdom_oidc_assume_role_policy" {
   statement {
     actions = ["sts:AssumeRoleWithWebIdentity"]
     effect  = "Allow"
 
     condition {
       test     = "StringEquals"
-      variable = "${replace(aws_iam_openid_connect_provider.eks.url, "https://", "")}:sub"
-      values   = ["system:serviceaccount:default:aws-test"]
+      variable = "${replace(aws_iam_openid_connect_provider.techdomeks.url, "https://", "")}:sub"
+      values   = ["system:serviceaccount:default:aws-techdom"]
     }
 
     principals {
-      identifiers = [aws_iam_openid_connect_provider.eks.arn]
+      identifiers = [aws_iam_openid_connect_provider.techdomeks.arn]
       type        = "Federated"
     }
   }
 }
 
-resource "aws_iam_role" "test_oidc" {
-  assume_role_policy = data.aws_iam_policy_document.test_oidc_assume_role_policy.json
-  name               = "test-oidc"
+resource "aws_iam_role" "techdom_oidc" {
+  assume_role_policy = data.aws_iam_policy_document.techdom_oidc_assume_role_policy.json
+  name               = "techdom-oidc"
 }
 
-resource "aws_iam_policy" "test-policy" {
-  name = "test-policy"
+resource "aws_iam_policy" "techdom-policy" {
+  name = "techdom-policy"
 
   policy = jsonencode({
     Statement = [{
@@ -386,12 +387,12 @@ resource "aws_iam_policy" "test-policy" {
 }
 
 resource "aws_iam_role_policy_attachment" "test_attach" {
-  role       = aws_iam_role.test_oidc.name
-  policy_arn = aws_iam_policy.test-policy.arn
+  role       = aws_iam_role.techdom_oidc.name
+  policy_arn = aws_iam_policy.techdom-policy.arn
 }
 
 output "test_policy_arn" {
-  value = aws_iam_role.test_oidc.arn
+  value = aws_iam_role.techdom_oidc.arn
 }
 
 
@@ -405,12 +406,12 @@ data "aws_iam_policy_document" "eks_cluster_autoscaler_assume_role_policy" {
 
     condition {
       test     = "StringEquals"
-      variable = "${replace(aws_iam_openid_connect_provider.eks.url, "https://", "")}:sub"
+      variable = "${replace(aws_iam_openid_connect_provider.techdomeks.url, "https://", "")}:sub"
       values   = ["system:serviceaccount:kube-system:cluster-autoscaler"]
     }
 
     principals {
-      identifiers = [aws_iam_openid_connect_provider.eks.arn]
+      identifiers = [aws_iam_openid_connect_provider.techdomeks.arn]
       type        = "Federated"
     }
   }
